52ff635d16c0eed0c942813b56284a7639d265b7
Hiki <hikiriki29@gmail.com>
2024-12-18 00:59:10 +0100

    added new function to add table as usd

--- Thesis/lib/setup_import_standart.py
+++ Thesis/lib/setup_import_standart.py
@@ -5,12 +5,13 @@ import numpy as np
 from omni.isaac.nucleus import get_assets_root_path
 
 from omni.isaac.core import World
-from omni.isaac.core.utils.prims import is_prim_path_valid
 from omni.isaac.core.utils.stage import get_stage_units
-from omni.isaac.core.utils.string import find_unique_string_name
+from omni.isaac.core.physics_context import PhysicsContext
+from omni.isaac.core.utils.prims import is_prim_path_valid
 from omni.isaac.core.utils.stage import add_reference_to_stage
-from omni.isaac.core.objects import VisualCuboid, DynamicCuboid
 from omni.isaac.core.utils.rotations import euler_angles_to_quat
+from omni.isaac.core.utils.string import find_unique_string_name
+from omni.isaac.core.objects import VisualCuboid, DynamicCuboid, FixedCuboid
 # import omni.isaac.core.controllers.articulation_controller.ArticulationController
 
 from omni.isaac.manipulators import SingleManipulator
